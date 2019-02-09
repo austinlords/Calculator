647b5a25dd48812ad2740aa629bd287a0744ec4a
Austin Lords <austinlords@outlook.com>
2019-02-09 11:36:21 -0800

    all 4 operations in 1 form

--- js/scripts.js
+++ js/scripts.js
@@ -16,37 +16,28 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+var number1;
+var number2;
+var operation;
+
 //front-end logic
 $(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculatorForm").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#addOutput").text("Result: " + result);
-  });
+    number1 = parseFloat($("#firstNumber").val());
+    number2 = parseFloat($("#secondNumber").val());
+    operation = $("input[type=radio]:checked").val();
 
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-    var result = subtract(number1, number2);
-    $("#subtractOutput").text("Result: " + result);
-  });
+    if (operation === "add") {
+      var result = number1 + number2;
+    } if (operation === "subtract") {
+      var result = number1 - number2;
+    } if (operation === "multiply") {
+      var result = number1 * number2
+    } if (operation === "divide") {
+      var result = number1 / number2
+    }
 
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-    var result = multiply(number1, number2);
-    $("#multiplyOutput").text("Result: " + result);
-  });
-
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
-    $("#divideOutput").text("Result: " + result);
+    $("div#result").text("Result: " + result);
   });
 });
